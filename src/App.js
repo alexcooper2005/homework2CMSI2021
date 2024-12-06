c7f62b320ef179cd44004813a039f7a591d5b04d
alexcooper2005 <143565766+alexcooper2005@users.noreply.github.com>
2024-12-06 15:04:49 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="theme-color" content="#000000">
-	<!--
-      manifest.json provides metadata used when your web app is added to the
-      homescreen on Android. See https://developers.google.com/web/fundamentals/engage-and-retain/web-app-manifest/
-    -->
-	<link rel="manifest" href="%PUBLIC_URL%/manifest.json">
-	<link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
- 
-	<!--
-      Notice the use of %PUBLIC_URL% in the tags above.
-      It will be replaced with the URL of the `public` folder during the build.
-      Only files inside the `public` folder can be referenced from the HTML.
+import { useEffect, useState } from "react";
+import "./styles.css";
+import Title from "./Title.js";
+import Entry from "./Entry.js";
+import Info from "./Info.js";
 
-      Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
-      work correctly both with client-side routing and a non-root public URL.
-      Learn how to configure a non-root public URL by running `npm run build`.
-    -->
-	<title>React App</title>
-</head>
+export default function App() {
+  const [name, setName] = useState(""); 
+  const [data, setData] = useState(null); 
 
-<body>
-  <link href="https://fonts.googleapis.com/css2?family=Sofia:wght@400&display=swap" rel="stylesheet">
-	<noscript>
-		You need to enable JavaScript to run this app.
-	</noscript>
-	<div id="root"></div>
-	<!--
-      This HTML file is a template.
-      If you open it directly in the browser, you will see an empty page.
+  useEffect(() => {
+    const amiiboName = encodeURIComponent(name.toLowerCase());
+    const url = `https://www.amiiboapi.com/api/amiibo/?name=${amiiboName}`;
+    
+    fetch(url)
+      .then((r) => {
+        return r.json();
+      })
+      .then((r) => {
+        if (r.amiibo && r.amiibo.length > 0) {
+          setData(r.amiibo[0]); 
+        } else {
+          setData(null); 
+        }
+      })
+      .catch((e) => {
+        console.error(e);
+        setData(null); 
+      });
+  }, [name]);
 
-      You can add webfonts, meta tags, or analytics to this file.
-      The build step will place the bundled scripts into the <body> tag.
-
-      To begin the development, run `npm start` or `yarn start`.
-      To create a production bundle, use `npm run build` or `yarn build`.
-    -->
-</body>
-
-</html>
+  return (
+    <div className="App">
+      <Title text="Amiibo Info Lister" />
+      <Entry action={setName} />
+      <Info name={name} data={data} />
+    </div>
+  );
+}
